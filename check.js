563927874bee16e3cd598f3fdeee58bacd06e9e2
kostyamessanger <kostyagameshelp@vk.ru>
2025-12-17 13:06:45 +0500

    Update check.js

--- check.js
+++ check.js
@@ -1,10 +1,16 @@
 function checkCaptcha() {
     const checkbox = document.getElementById('captchaCheckbox');
+    const spinner = document.querySelector('.spinner');
     const resultElement = document.getElementById('result');
 
     if (checkbox.checked) {
-        resultElement.innerText = 'Капча пройдена!';
+        spinner.style.display = 'block';
+        setTimeout(() => {
+            spinner.style.display = 'none';
+            resultElement.innerText = 'Капча пройдена!';
+        }, 10000);
     } else {
+        spinner.style.display = 'none';
         resultElement.innerText = 'Капча не пройдена. Попробуйте еще раз.';
     }
 }
