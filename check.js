2b24c5104a062f37d4d2d385a4a2154fc1dadcd4
kostyamessanger <kostyagameshelp@vk.ru>
2025-12-17 13:02:21 +0500

    Update check.js

--- check.js
+++ check.js
@@ -1,18 +1,11 @@
-document.querySelector('button').addEventListener('click', function() {
+function checkCaptcha() {
     const checkbox = document.getElementById('captchaCheckbox');
-    const spinner = document.querySelector('.spinner');
     const resultElement = document.getElementById('result');
 
     if (checkbox.checked) {
-        spinner.style.display = 'block';
-        // Здесь можно добавить код для взаимодействия с GitHub API
-        // Например, отправка запроса на проверку капчи
-        setTimeout(() => {
-            spinner.style.display = 'none';
-            resultElement.innerText = 'Капча пройдена!';
-        }, 10000);
+        resultElement.innerText = 'Капча пройдена!';
     } else {
         resultElement.innerText = 'Капча не пройдена. Попробуйте еще раз.';
     }
-});
+}
  
